6ca04bc02835df75e2edda1f6ccb5098ec5834ec
Faiza Hamid <hamid.faiza811@gmail.com>
2024-07-24 17:31:59 +0300

    added adaptive mixins for grid and container inline paddings

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
+import './style/main.scss';
 
 export const App: React.FC = () => {
 
   return (
-    <div>example</div>
+    <div className="App">
+      <main className="main">
+        <div className='container'>
+        </div>
+      </main>
+    </div>
   );
 };
 
