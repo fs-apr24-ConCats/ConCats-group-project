0b990ceec1fc2f12eeeaedd0410e8da1a00311c4
Faiza Hamid <hamid.faiza811@gmail.com>
2024-07-25 07:47:09 +0300

    joined all changes

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import classes from './Logo.module.scss';
 import { Link } from 'react-router-dom';
 
-export const Logo = () => (
+export const Logo: React.FC = () => (
   <Link to="/">
     <button type="button">
       <div className={classes.Logo}>
-        <img src="/img/icons/OK.png" className={classes.Logo__img} alt="Logo" />
+        <img src="../public/img/icons/OK.png" className={classes.Logo__img} alt="Logo" />
       </div>
     </button>
   </Link>
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,30 @@
-import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { Root } from './Root';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { App } from './App';
+import { HomePage } from './modules/HomePage';
 
-createRoot(document.getElementById('root') as HTMLElement).render(<Root />);
+createRoot(document.getElementById('root') as HTMLElement).render(
+  <Router>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route index element={<HomePage />} />
+        <Route path="home" element={<Navigate to={'/'} />} />
+
+        <Route path="phones" element={<h1>Phones</h1>}>
+          <Route path=":phoneId?" element={<h1>PhonesItems</h1>} />
+        </Route>
+
+        <Route path="tablets" element={<h1>Tablets</h1>}>
+          <Route path=":tabletId?" element={<h1>TabletsItems</h1>} />
+        </Route>
+
+        <Route path="accessories" element={<h1>Acсessories</h1>}>
+          <Route path=":accesorieId?" element={<h1>AcсessoriesItems</h1>} />
+        </Route>
+
+        <Route path="favorites" element={<h1>Favorites</h1>} />
+        <Route path="cart" element={<h1>Cart</h1>} />
+        <Route path="*" element={<h1>NotFoundPage</h1>} />
+      </Route>
+    </Routes>
+  </Router>);
