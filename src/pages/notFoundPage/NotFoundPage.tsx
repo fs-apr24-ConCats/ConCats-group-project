ef4c69ecc51432cfc75cca937f36f757d38e4636
Oleksii Knihin <alexyo2626@gmail.com>
2024-07-26 14:03:20 +0300

    change classes on module classes

--- src/pages/notFoundPage/NotFoundPage.tsx
+++ src/pages/notFoundPage/NotFoundPage.tsx
@@ -1,19 +1,20 @@
 import React from 'react';
-import './NotFoundPage.scss';
-// import notFoundImg from '../../../public/img/page-not-found.png';
+import classes from './NotFoundPage.module.scss';
 
 export const NotFoundPage: React.FC = () => {
   return (
-    <div className="pageNotFound__wrapper">
-      <div className="pageNotFound">
-        <div className="pageNotFound__image-wrapper">
+    <div className={classes.pageNotFound__wrapper}>
+      <div className={classes.pageNotFound}>
+        <div className={classes.pageNotFound__imageWrapper}>
           <img
             src="img/404-not-found.png"
             alt="page-not-found"
-            className="pageNotFound__img"
+            className={classes.pageNotFound__img}
           />
         </div>
-        <h1 className="pageNotFound__title">Oooops! Page not found...</h1>
+        <h1 className={classes.pageNotFound__title}>
+          Oooops! Page not found...
+        </h1>
       </div>
     </div>
   );
