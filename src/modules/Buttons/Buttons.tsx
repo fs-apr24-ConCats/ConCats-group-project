31fd52dda67008973f70a1a37a9a3a08b393486f
priemssonia <priemssonia@gmail.com>
2024-07-26 16:49:40 +0300

    Create an Card-Layout

---
+++ src/modules/Buttons/Buttons.tsx
@@ -0,0 +1,78 @@
+import classNames from 'classnames';
+import styles from './Buttons.module.scss';
+import { getId } from '../../styles/utils/getId';
+import { Product } from '../../types/Product';
+
+type Props = {
+  category: string;
+  id: string;
+  products: Product[];
+  favourites: Product[];
+  cart: Product[];
+  onAddToFavourites: (id: string) => void;
+  onAddToCart: (id: string) => void;
+};
+
+export const Buttons: React.FC<Props> = ({
+  category,
+  id,
+  products,
+  favourites,
+  cart,
+  onAddToFavourites,
+  onAddToCart,
+}) => {
+  const hasInCart = cart.some(item => item.id === getId(category, products, id));
+
+  const onClickFavHandle = () => {
+    const itemId = getId(category, products, id);
+    if (itemId !== undefined) {
+      onAddToFavourites(itemId.toString());
+    }
+  };
+
+  const onClickCart = () => {
+    const itemId = getId(category, products, id);
+    if (itemId !== undefined) {
+      onAddToCart(itemId.toString());
+    }
+  };
+
+  return (
+    <div className={classNames(styles.buttons)}>
+      {hasInCart ? (
+        <button
+          className={classNames(
+            styles.buttons__button,
+            styles['buttons__button-cart'],
+            styles['buttons__button-cartActive']
+          )}
+          onClick={onClickCart}
+        >
+          Added
+        </button>
+      ) : (
+        <button
+          className={classNames(
+            styles.buttons__button,
+            styles['buttons__button-cart']
+          )}
+          onClick={onClickCart}
+        >
+          Add to cart
+        </button>
+      )}
+
+      <button
+        className={classNames(
+          styles.buttons__button,
+          styles['buttons__button-fav'],
+          {
+            [styles['buttons__button-fav-selected']]: favourites.some(item => item.id === getId(category, products, id)),
+          }
+        )}
+        onClick={onClickFavHandle}
+      />
+    </div>
+  );
+};
--- src/modules/HomePage/HomePage.tsx
+++ src/modules/HomePage/HomePage.tsx
@@ -1,8 +1,44 @@
 import React from "react"
 import classes from './HomePage.module.scss';
+import { ProductCard } from '../../components/ProductCard';
+import { Product } from '../../types/Product';
 
 export const HomePage: React.FC = () => {
+  const exampleProduct: Product = {
+    id: 0,
+    category: "electronics",
+    itemId: "example-id",
+    name: "Example Product",
+    fullPrice: 1000,
+    price: 800,
+    screen: "6.1 inches",
+    capacity: "128GB",
+    color: "Black",
+    ram: "4GB",
+    year: 2022,
+    image: "example-image-url.jpg",
+  };
+
+  const handleAddToFavourites = (id: string) => {
+    console.log(`Added to favourites: ${id}`);
+  };
+
+  const handleAddToCart = (id: string) => {
+    console.log(`Added to cart: ${id}`);
+  };
+
     return(
+      <>
         <h1 className={classes.title}>Header ↑</h1>
+        <ProductCard
+          product={exampleProduct}
+          products={[exampleProduct]}
+          favourites={[exampleProduct]}
+          cart={[]}
+          onAddToFavourites={handleAddToFavourites}
+          onAddToCart={handleAddToCart}
+       />
+      </>
     )
+
 }
