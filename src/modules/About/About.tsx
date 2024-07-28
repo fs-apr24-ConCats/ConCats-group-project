5a259108a0b1e98abec98b94640fb55ff1c6761c
Faiza Hamid <158755424+FaiHamid@users.noreply.github.com>
2024-07-28 23:22:11 +0300

    Update About.tsx

--- src/modules/About/About.tsx
+++ src/modules/About/About.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import classes from './CartItem.module.scss';
+import classes from './About.module.scss';
 // import { Link } from 'react-router-dom';
 // import phones from '/api/phones.json';
 
