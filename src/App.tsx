439d4d70d851e37d59676928df221ee082df4db2
Faiza Hamid <hamid.faiza811@gmail.com>
2024-07-29 13:24:03 +0300

    merge card and homepage

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import './App.scss';
 import { Header } from './components/Header/Header';
 import { Outlet } from 'react-router-dom';
 import { Footer } from './components/Footer/Footer';
-import { Footer } from './components/Footer/Footer';
 
 export const App: React.FC = () => {
   return (
