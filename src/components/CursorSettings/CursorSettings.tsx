1490f7034646e532275153bc5876c417fc1da44a
priemssonia <priemssonia@gmail.com>
2024-08-05 18:46:16 +0300

    refresh

--- src/components/CursorSettings/CursorSettings.tsx
+++ src/components/CursorSettings/CursorSettings.tsx
@@ -2,25 +2,35 @@ import React, { useState, useEffect } from "react";
 import styles from './CursorSettings.module.scss';
 
 export const CursorSettings: React.FC = () => {
-  const [isCustomCursor, setIsCustomCursor] = useState(false);
+  const [isCustomCursor, setIsCustomCursor] = useState<boolean>(() => {
+    const saved = localStorage.getItem('customCursor');
+    return saved === 'true';
+  });
 
   useEffect(() => {
     console.log('Applying custom cursors');
+    
+    const elements = document.querySelectorAll('a, button, nav, ul, .custom-pointer, ._pageFormControl_1q2ih_215, .custom-arrow, .icon, ._settings_pxp5x_204, ._sortFormControl_1q2ih_214, ._Logo_1ffcl_204, ._buttons__button-cart_1jn3i_212, ._section_top_title_xleik_214, ._BreadcrumbsHome_1lywe_239, _Breadcrumbs__link_1lywe_200, li, ._pagination-arrowRight_q12qv_260, ._pagination-arrowLeft_q12qv_255, ._footer__backToTop_6568c_245,  ._settings_pxp5x_204, img, ._settings_pxp5x_204, ._menu_pxp5x_230, ._cursorSettings_1i0xv_204, label, ._product__image, ._slider_1i0xv_227, .menuItem');
+    const bodyElement = document.body;
 
-    const elements = document.querySelectorAll('a, button');
+    if (isCustomCursor) {
+      bodyElement.style.cursor = `url('/img/icons/okcursor.png'), auto`;
+      elements.forEach(el => {
+        const computedStyle = getComputedStyle(el);
+        if (computedStyle.cursor === 'pointer') {
+          (el as HTMLElement).style.cursor = `url('/img/icons/pointer-cursor.png'), pointer`;
+        } else {
+          (el as HTMLElement).style.cursor = computedStyle.cursor;
+        }
+      });
+    } else {
+      bodyElement.style.cursor = 'auto';
+      elements.forEach(el => {
+        (el as HTMLElement).style.cursor = 'pointer';
+      });
+    }
 
-    elements.forEach(el => {
-      const element = el as HTMLElement;
-      if (isCustomCursor) {
-        element.style.cursor = `url('/img/icons/okcursor.png'), auto`;
-      } else {
-        element.style.cursor = 'pointer';
-      }
-    });
-
-    document.body.style.cursor = isCustomCursor 
-      ? `url('/img/icons/okcursor.png'), auto` 
-      : 'auto';
+    localStorage.setItem('customCursor', String(isCustomCursor));
   }, [isCustomCursor]);
 
   const handleCustomCursorToggle = () => {
@@ -37,7 +47,3 @@ export const CursorSettings: React.FC = () => {
     </div>
   );
 };
-
-
-
-
