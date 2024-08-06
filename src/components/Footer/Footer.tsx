162b51a1fd3084912b3d8b06b2228959d1053b8e
priemssonia <priemssonia@gmail.com>
2024-08-06 03:22:16 +0300

    add route for rights

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -28,15 +28,15 @@ export const Footer: React.FC = () => {
         <div className={styles.container}>
           <Logo />
           <div className={styles.footer__links}>
-            <a className={styles.link} href="#">
+            <a className={styles.link} href="https://github.com/fs-apr24-ConCats/ConCats-group-project">
               GitHub
             </a>
             <Link className={styles.link} to="/contacts">
               {t('footer.contacts')}
             </Link>
-            <a className={styles.link} href="#">
+            <Link className={styles.link} to="/rights">
               {t('footer.rights')}
-            </a>
+            </Link>
           </div>
           <div className={styles.footer__backToTop}>
             <a onClick={scrollToTop} className={styles.footer__textInfo}>
